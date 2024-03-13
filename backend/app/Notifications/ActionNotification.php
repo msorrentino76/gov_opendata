7b873d673aba712daa7e9b4f4e763e089835994b
Marco Sorrentino <msorrentino.cinf@gmail.com>
2024-03-14 00:47:01 +0100

    notifiche via email

--- backend/app/Notifications/ActionNotification.php
+++ backend/app/Notifications/ActionNotification.php
@@ -3,51 +3,70 @@
 namespace App\Notifications;
 
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class ActionNotification extends Notification
 {
-
-    public $mittente;
-    public $azione;
-    public $id_entity;
-
+    
     const ATTIVITA = 'ACT';
     const VENDITA  = 'SELL';
     
     
-    //$user_destinatario->notify(new ActionNotification($user_che_ha_fatto_l'action, ActionNotification::ATTIVITA, $entita_appena_inserita->id));
-    
-    public function __construct($mittente, $azione, $id_entity)
-    {
-        $this->mittente  = $mittente;
-        $this->azione    = $azione;
-        $this->id_entity = $id_entity;
-    }
+    /* $user_destinatario->notify(
+     *      new ActionNotification(
+     *          $user_che_ha_fatto_action,
+     *          $user_destinatario,
+     *          ActionNotification::ATTIVITA,
+     *          $entita_appena_inserita->id,
+     *      )   
+     * );
+     */
 
-    public function via($notifiable)
-    {
-        return ['database'];
-    }
+    private $destinatario;
+    private $messaggio;
+    private $id_entity;
     
-    public function toDatabase($notifiable)
+    public function __construct($mittente, $destinatario, $azione, $id_entity)
     {
-        $message = '';
         
-        switch ($this->azione) {
+        $messaggio = '';
+        
+        switch ($azione) {
             
             case self::ATTIVITA :
-                $message = 'ha inserito l\'attività';
+                $messaggio = 'ha inserito l\'attività';
                 break;
 
             case self::VENDITA :
-                $message = 'ha inserito la vendita';
+                $messaggio = 'ha inserito la vendita';
                 break;
             
         }
         
+        $this->destinatario = $destinatario;
+        $this->messaggio = $mittente->name . ' ' . $mittente->surname . ' ' . $messaggio . ' #' . $id_entity;
+        $this->id_entity = $id_entity;
+        
+    }
+
+    public function via($notifiable) {
+        return ($this->destinatario->notify_email === true) ? ['mail', 'database'] : ['database'];
+    }
+    
+    public function toDatabase($notifiable) {
         return [
             'id' => $this->id_entity,
-            'message' => $this->mittente->name . ' ' . $this->mittente->surname . ' ' . $message . ' ' . $this->id_entity,
+            'message' => $this->messaggio,
         ];
     }
+    
+    public function toMail($notifiable){
+        return (new MailMessage)
+                    ->subject('Centro notifiche Numieport')
+                    ->greeting('Salve ' . $this->destinatario->name . ' ' . $this->destinatario->surname)
+                    ->line($this->messaggio)
+                    //->lineIf($this->amount > 0, "Amount paid: {$this->amount}")
+                    ->action('Accedi a sistema', config('app.url'));
+    }
+
 }
