1616f90b58799f1636dea7056b746a7279a20452
Marco Sorrentino <msorrentino.cinf@gmail.com>
2024-10-01 16:45:44 +0200

    filtro categorie

--- backend/app/Http/Controllers/api/v1/legalEntityAdmin/IstatController.php
+++ backend/app/Http/Controllers/api/v1/legalEntityAdmin/IstatController.php
@@ -10,10 +10,12 @@ class IstatController extends Controller
 
     public function index(){
         
-        $resp = $this->_http('https://sdmx.istat.it/SDMXWS/rest/dataflow/IT1');
+        $url = 'https://sdmx.istat.it/SDMXWS/rest/dataflow/IT1';
+        
+        $resp = $this->_http($url);
         
         if($resp['error']){
-            Log::error("IstatController:index - " . $resp['httpCode'] . ' ' . $resp['errorMessage'] );
+            Log::error("IstatController:index - $url - " . $resp['httpCode'] . ' ' . $resp['errorMessage'] );
             return response()->json('Errore durante l\'interrogazione ISTAT', 500);
         }
         
@@ -21,17 +23,38 @@ class IstatController extends Controller
         
         $array = json_decode(substr($resp['content'], $i));
 
-        $dataset = [];
+        $dataflow = [];
         
         foreach($array->references as $reference){
             $id            = $reference->id;
             $name          = $reference->name;
+            $version       = $reference->version;
+            $is_final      = $reference->isFinal;
             $structure_urn = $reference->structure->urn;
-            $dataset[] = ['id' => $id, 'name' => $name];
+            $data_struct   = $this->removeTextBetweenParentheses(explode(':', $structure_urn)[3]);
+            $category      = explode('_', $reference->id)[0];
+            $dataflow[] = ['id' => $id, 'category' => $category, 'data_struct' => $data_struct, 'is_final' => $is_final, 'name' => $name . " (ver. $version)"];
         }
         
         //file_put_contents('output.txt', $resp['content']);
         
+        $url = 'https://sdmx.istat.it/SDMXWS/rest/categoryscheme/IT1';
+        
+        $resp = $this->_http($url);
+        
+        if($resp['error']){
+            Log::error("IstatController:index - $url - " . $resp['httpCode'] . ' ' . $resp['errorMessage'] );
+            return response()->json('Errore durante l\'interrogazione ISTAT', 500);
+        }
+        
+        $i = strpos($resp['content'], '{');
+        
+        $array = json_decode(substr($resp['content'], $i));
+        
+        $category_schema = 'urn:sdmx:org.sdmx.infomodel.categoryscheme.CategoryScheme=IT1:ISTAT_DW(1.0)';
+        
+        $categories = $this->_recursive_category($array->references->{$category_schema}->items);
+
         /**
          * @todo
          * 1- questo processo andrà ad alimentare il DB dataset STORICIZZATO
@@ -40,7 +63,12 @@ class IstatController extends Controller
          * 4- l'admin avrà contezza dell'esito dei processi (se e cosa andrà aggiornato)
          * 5- in caso di aggiornamenti/creazione partono notifiche
          */
-        return response()->json($dataset, 200); 
+        
+        usort($categories, function($a, $b) {
+            return strcmp($a['label'], $b['label']);
+        });
+
+        return response()->json(['dataflow' => $dataflow, 'categories' => $categories], 200); 
     }
 
     private function _http($url) {
@@ -67,4 +95,20 @@ class IstatController extends Controller
         return $resp;
     }
         
+    private function _recursive_category($items) {
+        $cats = [];
+        foreach ($items as $item) {            
+            $res['value']   = $item->id;
+            $res['label'] = $item->name;
+            if(isset($item->items)){
+                $res['children'] = $this->_recursive_category($item->items);
+            }
+            $cats[] = $res;
+        }
+        return $cats;
+    }
+    
+    private function removeTextBetweenParentheses($string) {
+        return preg_replace('/\s*\(.*?\)\s*/', '', $string);
+    }
 }
--- frontend/src/views/legaladmin/Dataset.vue
+++ frontend/src/views/legaladmin/Dataset.vue
@@ -4,18 +4,44 @@
     
     <el-card class="box-card">
 
-      <div style="padding: 16px 0 16px 0;">Risultati: {{ filterTableData.length }}</div>
+      <el-row :gutter="20">
+
+        <el-col :span="12">
+          <div class="content">Ricerca testo:</div>
+          <el-input v-model="search" placeholder="Inserisci testo..." />
+        </el-col>
+
+        <el-col :span="12">
+          <div class="content">Filtra per categoria:</div>
+          <el-tree-select
+            v-model="filter_by_cat"
+            :data="categories"
+            multiple
+            :render-after-expand="false"
+            show-checkbox
+            check-strictly
+            check-on-click-node
+            popper-append-to-body="false"
+            style="width: 100%"
+          />
+        </el-col>
+
+      </el-row>
+
+      <br><br>
+
+      <div class="content">Risultati: {{ filterTableData.length }}</div>
 
       <el-table :data="filterTableData" style="width: 100%" v-loading="loading" empty-text="Nessun risultato trovato" :default-sort="{ prop: 'name', order: 'asc' }" >
 
-        <!--el-table-column prop="id"    label="id"/-->
-        <el-table-column prop="name"  label="Categoria"/>
+        <!--
+        <el-table-column prop="id"           label="id"/>
+        <el-table-column prop="category"     label="Categoria"/>
+        <el-table-column prop="data_struct"  label="Data struct"/>
+        <el-table-column prop="is_final"     label="Finale"/>
+        -->
 
-        <el-table-column align="right">
-          <template #header>
-            <el-input v-model="search" size="small" placeholder="Cerca..." />
-          </template>
-        </el-table-column>
+        <el-table-column prop="name"         label="Dati disponibili"/>
 
       </el-table>
 
@@ -29,20 +55,44 @@
 
   import {list} from '../../utils/service.js'
 
-  const loading   = ref(false);
-  const search    = ref('');
-  const dataset = ref([]);
+  const loading    = ref(false);
+  const search     = ref('');
+  const dataflow   = ref([]);
+  const categories = ref([]);
+
+  const filter_by_cat = ref();
 
     const filterTableData = computed(() =>
-      dataset.value.filter(
-        (data) => !search.value || data.name.toLowerCase().includes(search.value.toLowerCase())
+      dataflow.value.filter(
+        (data) => {
+          if (!search.value && filter_by_cat.value.length == 0) {
+            return data;
+          }
+          if (search.value && filter_by_cat.value.length != 0) {
+            if (data.name.toLowerCase().includes(search.value.toLowerCase()) && filter_by_cat.value.includes(data.category) ){
+              return data;
+            }
+          }
+          if (search.value && filter_by_cat.value.length == 0) {
+            if (data.name.toLowerCase().includes(search.value.toLowerCase())){
+              return data;
+            }
+          }
+          if (!search.value && filter_by_cat.value.length != 0) {
+            if (filter_by_cat.value.includes(data.category) ){
+              return data;
+            }
+          }
+        }
       )
     )
 
     onMounted(async ()=>{
       loading.value = true;
 
-      dataset.value = await list('le_admin/dataset');
+      let dataset = await list('le_admin/dataset');
+      dataflow.value   = dataset.dataflow;
+      categories.value = dataset.categories;
 
       loading.value = false;
     });
